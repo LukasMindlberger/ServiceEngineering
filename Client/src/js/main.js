5167b7605905aec12ca14175627e6faedc84b940
robert <robert.oerdoeg@gmail.com>
2016-11-11 21:22:57 +0100

    Big update
    Registration
    Login

--- Client/src/js/main.js
+++ Client/src/js/main.js
@@ -1,8 +1,15 @@
 (function(){
-  var app = angular.module('blogService',[]);
+  var app = angular.module('blogService',['ui.bootstrap']);
   
   
   app.controller('BlogController', ['$scope', '$http', '$window', function($scope, $http, $window){
+
+    var ip = "http://127.0.0.1";
+    var port = ":8081";
+    var url = ip+port;
+
+
+
     
     var blog = this;
     blog.title = "Really really cool blog service app";
@@ -12,7 +19,7 @@
       blog.posts = data;
     });
 	
-    blog.tab = 'blog';
+    blog.tab = 'login';
 	
     blog.selectTab = function(setTab){
       blog.tab = setTab;
@@ -25,6 +32,11 @@
     
     blog.post = {};
     blog.addPost = function(){
+      var post =
+
+
+
+
       blog.post.createdOn = Date.now();
       blog.post.comments = [];
       blog.post.likes = 0;
@@ -32,35 +44,104 @@
       blog.tab = 0;
       blog.post ={};
       //to send
-    };   
+    };
 
-    blog.registrate = function(){
-      blog.username = "";
-      blog.password ="";
-      blog.email = "";
-      //to send
-    }
+
+
     
     blog.members=[];
+
+    blog.allMembers = ["Robert", "Christian", "Thomas"];
+    blog.getAllMembers = function(){
+
+    };
     
     blog.addMember = function(newMember){
     	blog.members.push(newMember);
     	blog.newMember="";
     	//to send
-    }
+    };
+
+
+    blog.deleteMember = function(member){
+        blog.members.splice(blog.members.indexOf(member),1);
+    };
     
-    
-    blog.user="";
-    
-    blog.registrate = function(){
-    	if(blog.newUsername == null || blog.newPassword == null || blog.newPassword2 == null || blog.newEmail == null ){
+
+
+    blog.registrate = function(username, password, password2, emailaddress){
+    	if(username == null || password == null || password2 == null || emailaddress == null ){
     		$window.alert("Please fill in required information!");
-    	}else if(blog.newPassword != blog.newPassword2){
+    	}else if(password != password2){
     		$window.alert("Passwords are not equal!");
     	}else{
-    		$window.alert("Registration complete!");
+
+            var user = {};
+            user.Name = username;
+            user.Password = password;
+            user.EMail = emailaddress;
+
+            $http.post(url+'/addUser', user)
+                .success(function(data) {
+                  console.log(data);
+                  $window.alert("Registration was successfull!");
+                })
+                .error(function(data){
+                  console.log(data);
+                  $window.alert("A user with this email address is already registrated.");
+                });
+
     	}
+    };
+
+    blog.currentUser = {};
+    blog.login = {};
+    blog.userLogin = function() {
+
+      var login = {};
+      login.EMail = blog.login.userEmailaddress;
+      login.Password = blog.login.userPassword;
+
+      blog.userLoggedIn = false;
+      $http.post(url + '/login', login)
+          .success(function (data) {
+            blog.tab = 'blog';
+            blog.userLoggedIn = true;
+            blog.currentUser = data;
+          })
+          .error(function (data) {
+            $window.alert("Login failed, please try again!");
+          });
+    };
+
+
+    blog.userLogout = function(){
+      blog.currentUser = null;
+      blog.userLoggedIn = false;
     }
+
+    blog.addGroup = function(){
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+    blog.status = {
+      isopen: false
+    };
+
+    blog.status = {
+      isopen2: false
+    };
+
     
 
   }]);
