2e0e5f71e35ecbb05ede2c465eecb03c498b74e2
robert <robert.oerdoeg@gmail.com>
2016-11-14 02:14:51 +0100

    update

--- Client/src/js/main.js
+++ Client/src/js/main.js
@@ -9,62 +9,24 @@
     var url = ip+port;
 
 
-
-    
     var blog = this;
-    blog.title = "Really really cool blog service app";
-    
+    blog.title = "Wi(n)Blog";
+
+    blog.post = {};
     blog.posts = {};
-    $http.get('example.json').success(function(data){
-      blog.posts = data;
-    });
 	
     blog.tab = 'login';
 	
     blog.selectTab = function(setTab){
+      if(setTab == "blog"){
+        blog.getPosts();
+      }
       blog.tab = setTab;
       console.log(blog.tab)
     };
     
     blog.isSelected = function(checkTab){
       return blog.tab === checkTab;
-    };
-    
-    blog.post = {};
-    blog.addPost = function(){
-      var post =
-
-
-
-
-      blog.post.createdOn = Date.now();
-      blog.post.comments = [];
-      blog.post.likes = 0;
-      blog.posts.unshift(this.post);
-      blog.tab = 0;
-      blog.post ={};
-      //to send
-    };
-
-
-
-    
-    blog.members=[];
-
-    blog.allMembers = ["Robert", "Christian", "Thomas"];
-    blog.getAllMembers = function(){
-
-    };
-    
-    blog.addMember = function(newMember){
-    	blog.members.push(newMember);
-    	blog.newMember="";
-    	//to send
-    };
-
-
-    blog.deleteMember = function(member){
-        blog.members.splice(blog.members.indexOf(member),1);
     };
     
 
@@ -95,6 +57,7 @@
     };
 
     blog.currentUser = {};
+
     blog.login = {};
     blog.userLogin = function() {
 
@@ -105,34 +68,122 @@
       blog.userLoggedIn = false;
       $http.post(url + '/login', login)
           .success(function (data) {
-            blog.tab = 'blog';
             blog.userLoggedIn = true;
             blog.currentUser = data;
+            blog.selectTab("blog");
           })
           .error(function (data) {
             $window.alert("Login failed, please try again!");
           });
-    };
 
+      blog.login.userEmailaddress = "";
+      blog.login.userPassword = "";
+    };
 
     blog.userLogout = function(){
       blog.currentUser = null;
       blog.userLoggedIn = false;
-    }
+      blog.tab = 'login'
+    };
 
-    blog.addGroup = function(){
+    blog.members = [];
+    blog.allMembers = {};
+    blog.getAllMembers = function(){
+      $http.post(url+'/listUsers', "")
+          .success(function(data){
+            blog.allMembers = data;
+          });
+    };
 
+    blog.addMember = function(newMember){
+      blog.members.push(newMember);
+      blog.newMember="";
+    };
+
+    blog.deleteMember = function(member){
+      blog.members.splice(blog.members.indexOf(member),1);
+    };
+
+    blog.createGroup = function(){
+      var newGroup = {};
+
+      if(blog.newGroupTitle != null && blog.newGroupTitle != "") {
+        newGroup.Name = blog.newGroupTitle;
+        newGroup.Participants = [];
+        for (mem in blog.members) {
+          var user2 = {};
+          user2.Name = blog.members[mem].Name;
+          user2.EMail = blog.members[mem].EMail;
+          newGroup.Participants[mem] = user2;
+        }
+
+        $http.post(url + '/addGroup', newGroup)
+            .success(function (data) {
+              $window.alert("Group created!");
+            })
+            .error(function (data) {
+              $window.alert("Group not created, no members!");
+            })
+      }else{
+        $window.alert("Group not created, please fill in a group title!");
+      }
+    };
+
+
+    blog.allGroups = {};
+    blog.getAllGroups = function(){
+        var mail = {};
+        mail.EMail = blog.currentUser.EMail;
+      $http.post(url + '/listGroups', mail)
+          .success(function(data){
+            blog.allGroups = data;
+          });
+    };
+
+    blog.createPostInGroup = "public";
+    blog.selectedGroup = function(groupName){
+      blog.createPostInGroup = groupName;
+    };
+
+
+    blog.addPost = function(){
+      var postToSend = {};
+      postToSend.Title = blog.newPostTitle;
+      postToSend.Text = blog.newPostBody;
+      postToSend.AuthorMail = blog.currentUser.EMail;
+      postToSend.AuthorName = blog.currentUser.Name;
+      postToSend.Date = String(Date.now());
+      postToSend.GroupID = blog.createPostInGroup;
+      $http.post(url + '/createBlogEntity', postToSend)
+          .success(function(data){
+            blog.getPosts();
+            $window.alert("Post created!");
+            blog.newPostTitle = "";
+            blog.newPostBody = "";
+          })
+          .error(function(data){
+            $window.alert("Post not created!");
+          });
     };
 
 
 
+      blog.comments = {};
+      blog.currentPost = {};
+    blog.storeCurrentPost = function(post){
+        blog.currentPost = post;
+        blog.getComments();
+    };
 
 
-
-
-
-
-
+    blog.getPosts = function(){
+      var mail = {};
+      mail.EMail = blog.currentUser.EMail;
+      $http.post(url + '/getBlogEntities', mail)
+          .success(function(data){
+            blog.posts = data;
+          })
+    };
 
     blog.status = {
       isopen: false
@@ -142,17 +193,35 @@
       isopen2: false
     };
 
-    
+    blog.status = {
+      isopen3: false
+    };
+
+    blog.addComment = function(){
+      var comment = {};
+      comment.BlogID = blog.currentPost.ID;
+      comment.Text = blog.commentText;
+      comment.AuthorMail = blog.currentUser.EMail;
+        comment.AuthorName = blog.currentUser.Name;
+      comment.Date = Date.now();
+
+        $http.post(url + '/addComment', comment)
+            .success(function(data){
+                $window.alert("Comment added!");
+                blog.getComments();
+            })
+
+    }
+
+
+      blog.getComments = function(){
+          var id = {};
+          id.ID = blog.currentPost.ID;
+          $http.post(url + '/listComments', id)
+              .success(function(data){
+                  blog.comments = data;
+              })
+      }
 
   }]);
-  
-  app.controller('CommentController', function(){
-    this.comment = {};
-    this.addComment = function(post){
-      this.comment.createdOn = Date.now();
-      post.comments.push(this.comment);
-      this.comment ={};
-    };
-  });
- 
 })();
